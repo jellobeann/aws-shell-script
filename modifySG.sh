9aff222035730ed1cee8f6a93d315f1446985290
Jobelle Bodestyne <ann.bodestyne@xurpas.com>
2018-09-04 16:00:57 +0800

    2nd commit

--- modifySG.sh
+++ modifySG.sh
@@ -6,5 +6,7 @@ cidrtxt="/home/ec2-user/cidrlist.txt"
 ips=`cat $cidrtxt | awk '{print $1}'`
 for item in $ips
 do
-aws ec2 authorize-security-group-ingress --group-id sg-09974598bd06aa1f0 --ip-permissions IpProtocol=tcp,FromPort=0,ToPort=65535,IpRanges="[{CidrIp=$item}]"
-done# modifyawssg
+aws ec2 authorize-security-group-ingress --group-id sg-09974598bd0 --ip-permissions IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges="[{CidrIp=$item}]"
+done
+
+# modifyawssg
