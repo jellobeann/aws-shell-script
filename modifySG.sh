e32bc3204006de09eba5e0bbcc5ab4821acda7fb
Jobelle Bodestyne <ann.bodestyne@xurpas.com>
2018-10-05 13:31:53 +0800

    modify security group

--- modifySG.sh
+++ modifySG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### Shell script that will modify/update an aws security group and open the specific port/all port for a list of IP.
+### Shell script that will modify/update an AWS Security Group and open the specific port/all port for a list of IP.
 
 cidrtxt="/home/ec2-user/cidrlist.txt"
 ips=`cat $cidrtxt | awk '{print $1}'`
@@ -8,5 +8,3 @@ for item in $ips
 do
 aws ec2 authorize-security-group-ingress --group-id sg-09974598bd0 --ip-permissions IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges="[{CidrIp=$item}]"
 done
-
-# modifyawssg
